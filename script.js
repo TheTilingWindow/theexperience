9b03fa32d933cd1b9c6ea8b1247917e00bdd8c84
TheTilingWindow <aussiekareem@gmail.com>
2025-12-14 23:05:14 +1100

    newfeatureandkuwait

--- script.js
+++ script.js
@@ -15,25 +15,42 @@ function change(name) {
                 flag.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Flag_of_Saudi_Arabia.svg/1200px-Flag_of_Saudi_Arabia.svg.png"
                 title.innerHTML = "This is Saudi Arabia!"
                 parent.style.backgroundImage = 'url("saudi.jpg")'
+                title.style.color = "#000000"
                 break;
                 case 'pl':
                     place.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6e351cc0-ecd2-4082-b2f6-e4d5497c7bc5/d90ttbz-0c684055-8810-4406-82a4-bec9d3b8b01f.png/v1/fill/w_1024,h_527,strp/al_quds_by_jabernoimi_d90ttbz-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTI3IiwicGF0aCI6IlwvZlwvNmUzNTFjYzAtZWNkMi00MDgyLWIyZjYtZTRkNTQ5N2M3YmM1XC9kOTB0dGJ6LTBjNjg0MDU1LTg4MTAtNDQwNi04MmE0LWJlYzlkM2I4YjAxZi5wbmciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.OARwgJkAArY5c2N7n_KLBcjXNtci0-XFBqua2t3CAO8"
                     flag.src = "https://myflag.com.au/wp-content/uploads/palestine-national-flag.jpg"
                     title.innerHTML = "This is Palestine"
                     parent.style.backgroundImage = 'url("palestine.jpg")'
+                    title.style.color = "#000000"
                     break;
                     case 'jo':
                         place.src = "jn-place.png"
                         flag.src = "https://upload.wikimedia.org/wikipedia/commons/c/c0/Flag_of_Jordan.svg"
                         title.innerHTML = "This is Jordan!"
                         parent.style.backgroundImage = 'url("jordanbg.jpg")'
+                        title.style.color = "#000000"
                         break;
                         case 'jp':
                             place.src = "jp-place.png"
                             flag.src = "https://upload.wikimedia.org/wikipedia/commons/9/9e/Flag_of_Japan.svg"
-                            title.innerHTML = "This is Japan"
+                            title.innerHTML = "This is Japan!"
                             parent.style.backgroundImage = 'url("japanbg.jpg")'
                             title.style.color = "#FFFFFF"
                             break;
-    }
+                            case 'kw':
+                                place.src = "kwplace.png"
+                                flag.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Flag_of_Kuwait.svg/500px-Flag_of_Kuwait.svg.png"
+                                title.innerHTML = "This is Kuwait!"
+                                parent.style.backgroundImage = 'url("https://content.r9cdn.net/rimg/dimg/97/e9/bbcce1e6-ctry-133-16cd86277ef.jpg")'
+                                title.style.color = "#000000"
+    }         
+}
+
+function hidemenu() {
+    document.getElementById('gflag').hidden = true;
+}
+
+function openmenu() {
+    document.getElementById('gflag').hidden = false;
 }
